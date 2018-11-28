98aa60d1c93582c8bff3c9a00cdbcedc69e76fe3
Efrat <efratraube@gmail.com>
2018-11-28 10:21:08 +0200

    new link

--- model.py
+++ model.py
@@ -3,6 +3,9 @@ import pprint
 import requests
 import wikipedia
 
+
+
+# f"https://en.wikipedia.org/w/api.php?action=query&titles={pageid}&prop=revisions&rvprop=content&rvsection=0&format=json")
 # blobtext = requests.get(f"https://en.wikipedia.org/wiki/Wikipedia:Multiyear_ranking_of_most_viewed_pages")
 # print(blobtext.text)
 # var titleRegex = new RegExp("<a href=\"/browse/post/\\d*/\">([^(]*) \\(");
