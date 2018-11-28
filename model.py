13a65f04561e0a3a7b98a0c869a90daa20704e49
Efrat <efratraube@gmail.com>
2018-11-28 19:53:04 +0200

    adding random values to questions

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 import pprint
+import random
+
 import requests
 import wikipedia as wp
 import re
@@ -6,6 +8,8 @@ import settings
 from pymongo.mongo_client import MongoClient
 import pandas
 import csv
+from collections import defaultdict
+
 
 class PagesDB:
     def __init__(self):
@@ -18,10 +22,14 @@ class PagesDB:
             reader = csv.reader(f, delimiter=",")
             for i, line in enumerate(reader):
                 # print('line[{}] = {}'.format(i, line))
-                self.lists.replace_one({"title": line[0].lower()}, {"title": line[0].lower()},upsert=True)
+                self.lists.replace_one({"title": line[0].lower()}, {"title": line[0].lower()}, upsert=True)
 
     def get_page(self, word):
         return self.lists.find_one({"title": word})
+
+    def get_random_page(self):
+        count = self.lists.estimated_document_count()
+        return self.lists.find()[random.randrange(count)]['title']
 
 
 class WordsDB:
@@ -56,60 +64,70 @@ class UsersDB:
         return score
 
     def update_score(self, id, score):
-        self.lists.replace_one({"user_id": id}, {"user_id": id, "score": self.get_score(id)+score}, upsert=True)
+        self.lists.replace_one({"user_id": id}, {"user_id": id, "score": self.get_score(id) + score}, upsert=True)
 
 
 class GameLogic:
     def __init__(self):
         self.w_db = WordsDB()
-        self.w_db.add_all_to_DB()
+        # self.w_db.add_all_to_DB()
         self.p_db = PagesDB()
-        self.p_db.add_all_to_DB()
+        # self.p_db.add_all_to_DB()
         self.user_db = UsersDB()
-        self.users_state_dict = {}
-        self.users_info_dict = {}
+        self.users_info_dict = defaultdict()
 
     def add_user(self, id):
         self.user_db.add_to_DB(id)
-        self.users_state_dict[id]="getting value"
-        self.users_info_dict[id] = {"good_guesses": 0, "wrong_guesses": 0, "score": 0, "played_guesses": []}
+        # self.users_state_dict[id] = "getting value"
+        # self.users_info_dict[id] = {"good_guesses": 0, "wrong_guesses": 0, "score": 0, "played_guesses": []}
 
-    def get_value(self,id):
-        value = wp.page("Donald Trump")
-        self.users_info_dict[id]['value']=value
-        return value
+        self.users_info_dict[id] = defaultdict()
+        self.users_info_dict[id]['state'] = "getting value"
+        self.users_info_dict[id]['good_guesses'] = 0
+        self.users_info_dict[id]['wrong_guesses'] = 0
+        self.users_info_dict[id]['score'] = 0
+        self.users_info_dict[id]['played_guesses'] = []
+
+
+    def get_full_page(self, id):
+        value = wp.page(self.users_info_dict[id]['page_title'])
+        self.users_info_dict[id]['page_content'] = value
+
+    def get_page_title(self,id):
+        self.users_info_dict[id]['page_title'] = self.p_db.get_random_page()
+        return self.users_info_dict[id]['page_title']
 
     def string_found(self, string1, string2):
         if re.search(r"\b" + re.escape(string1) + r"\b", string2):
             return True
         return False
 
-
     def test_word(self, word, id):
-        word=word.lower()
+        word = word.lower()
 
-        if self.users_state_dict[id]=="getting value":
+        if self.users_info_dict[id]['state'] == "getting value":
             if word == "yes":
-                self.users_state_dict[id] = "playing"
+                self.users_info_dict[id]['state'] = "playing"
+                self.get_full_page(id)
                 return "Ok. Try and guess 5 main words about him!"
             elif word == "no":
-                value=self.get_value(id)
-                return f"So you heard about {value.title}?"
+                self.get_page_title(id)
+                return f"So have you heard about {self.users_info_dict[id]['page_title']}?"
             else:
                 return "invalid answer.... answer 'yes' or 'no'."
 
-        value=self.users_info_dict[id]['value']
+
         if word in settings.LETTERS or set(word).issubset(settings.NUMBERS):
             return "Are you kidding? you can't guess numbers or letters...."
 
-        if self.string_found(word, value.content.lower()):
-            split=word.split()
+        if self.string_found(word, self.users_info_dict[id]['page_content'].content.lower()):
+            split = word.split()
             for w in split:
-                if w in self.users_info_dict[id]["played_guesses"] :
+                if w in self.users_info_dict[id]["played_guesses"]:
                     return "Nice try. can't fool me. you used this word already"
 
             if self.w_db.get_word(word):
-                return "C'mon,This word is way to common......"
+                return "C'mon,This word is way too common......"
 
             [self.users_info_dict[id]["played_guesses"].append(w) for w in split]
             self.users_info_dict[id]["good_guesses"] += 1
@@ -126,4 +144,3 @@ class GameLogic:
                 self.user_db.update_score(id, self.users_info_dict[id]['score'])
                 return f"Nah, You failed this round.\n Your score is {self.user_db.get_score(id)}"
             return f"Nope! You're wrong. tries left: {settings.NUM_WRONG_GUESSES -self.users_info_dict[id]['wrong_guesses']}"
-
