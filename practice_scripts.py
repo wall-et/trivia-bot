0655af9b4313cfcac9f3b5f6acfe3de889234203
Efrat <efratraube@gmail.com>
2018-11-28 20:24:34 +0200

    adding new values for db 2014-2015

--- practice_scripts.py
+++ practice_scripts.py
@@ -28,6 +28,33 @@ table_dataframe_minimal16 = table_dataframe16[['Article', 'views']]
 with open('minimal_data.csv', 'a', encoding='utf-8') as f:
     table_dataframe_minimal16.to_csv(f, header=None, index=False)
 
+
+html15 = requests.get(f"https://en.wikipedia.org/wiki/User:West.andrew.g/2015_Popular_pages")
+df15 = pd.read_html(html15.content)
+table_dataframe15 = df15[1][1:]
+column_names15 = ["Rank", "Article", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "views",
+                  "perc1", "perc2"]
+table_dataframe15.columns = column_names15
+table_dataframe_minimal15 = table_dataframe15[['Article', 'views']]
+# print(table_dataframe_minimal16)
+
+with open('minimal_data.csv', 'a', encoding='utf-8') as f:
+    table_dataframe_minimal15.to_csv(f, header=None, index=False)
+
+
+
+html14 = requests.get(f"https://en.wikipedia.org/wiki/User:West.andrew.g/2014_Popular_pages")
+df14 = pd.read_html(html14.content)
+table_dataframe14 = df14[1][1:]
+column_names14 = ["Rank", "Article", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "views",
+                  "perc1",]
+table_dataframe14.columns = column_names14
+table_dataframe_minimal14 = table_dataframe14[['Article', 'views']]
+# print(table_dataframe_minimal16)
+
+with open('minimal_data.csv', 'a', encoding='utf-8') as f:
+    table_dataframe_minimal14.to_csv(f, header=None, index=False)
+
 # with open("minimal_data.csv", "r", encoding='utf-8') as f:
 #     reader = csv.reader(f, delimiter=",")
 #     for i, line in enumerate(reader):
